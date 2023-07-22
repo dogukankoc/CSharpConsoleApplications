b98410f14d413afc9835d6d3c10182e9739cd949
Dogukan Koc <dk.dogukankoc@gmail.com>
2023-07-22 16:26:55 +0300

    CreatedCSharpConsoleApplicationsRepositoryandAddedDefaultKeywordConsoleApp

---
+++ DefaultKeyword/DefaultKeyword/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Reflection;
+//I wanted to show  the default values ​​of the Value and Reference Types
+
+namespace DefaultKeyword
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int intNumber = default;
+            Console.WriteLine($"Integer default value = {intNumber}\n");
+
+            //Value Data Types
+            Console.WriteLine($"byte: {default(byte)}");
+            Console.WriteLine($"int: {default(int)}");
+            Console.WriteLine($"short: {default(short)}");
+            Console.WriteLine($"long: {default(long)}");
+            Console.WriteLine($"float: {default(float)}");
+            Console.WriteLine($"double: {default(double)}");
+            Console.WriteLine($"decimal {default(decimal)}");
+            Console.WriteLine($"bool: {default(bool)}"); //false
+            Console.WriteLine($"char: {default(char)}"); // \u0000 => char default value is a null character
+            Console.WriteLine("char: \u0000"); // Char default value 
+            Console.WriteLine($"enum: {default(A)}"); // default E(0) = 0
+            Console.WriteLine($"enum: {default(B)}"); // E(0) = Example
+
+            //Reference Data Types
+            Console.WriteLine($"\nstring: {default(string)}"); //null
+            Console.WriteLine($"object: {default(object)}"); //null
+            Console.WriteLine($"dynamic: {default(dynamic)}"); //null
+            Console.WriteLine($"array: {default(Array)}"); //null
+            Console.WriteLine($"Delegate: {default(Delegate)}"); //null
+            Console.WriteLine($"Pointer: {default(Pointer)}\n"); //null
+
+            if (default(char) == null)
+            {
+                Console.WriteLine("Char type is null"); //Unreachable code
+            }
+            else
+            {
+                Console.WriteLine("Char type is not null");
+            }
+
+            if (default(string) == null)
+            {
+                Console.WriteLine("String type is null");
+            }
+            else
+            {
+                Console.WriteLine("String type is not null"); //Unreachable code
+            }
+
+            if (default(object) == null)
+            {
+                Console.WriteLine("Object type is null");
+            }
+
+            if (default(dynamic) == null)
+            {
+                Console.WriteLine("Dynamic type is null");
+            }
+
+            if (default(Array) == null)
+            {
+                Console.WriteLine("Array type is null");
+            }
+            if (default(Delegate) == null)
+            {
+                Console.WriteLine("Delegate type is null");
+            }
+
+            if (default(Pointer) == null)
+            {
+                Console.WriteLine("Pointer type is null");
+            }
+
+            if (default(Enum) == null)
+            {
+                Console.WriteLine("Enum type is null");
+            }
+        }
+        enum A
+        {
+            Foo = 1, Bar = 2, Baz = 3, Quux = 4
+        }
+        enum B
+        {
+            Foo = 1, Bar = 2, Baz = 3, Quux = 4, Example = 0
+        }
+    }
+}
---
+++ readme.md
@@ -0,0 +1,7 @@
+### Hello World
+
+In this repository I will share my C# console applications. I hope you can benefit this repo ![image](https://github.com/dogukankoc/30DaysOfJavaScriptExercises/assets/109290790/1ff49acb-d7a4-4d5d-9ca9-1934b6d15de6)
+</br>
+</br>
+
+![image](https://www.syncfusion.com/blogs/wp-content/uploads/2019/11/csharp-using-declaration.png)
