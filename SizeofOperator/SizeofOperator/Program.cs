c3ab8c01cad90f6238d0820971fc4f5a12073c2a
Dogukan Koc <dk.dogukankoc@gmail.com>
2023-07-31 21:49:58 +0300

    SizeOfConsoleAppCreated

---
+++ SizeofOperator/SizeofOperator/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace SizeofOperator
+{ 
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //sizeof operator: Returns an int holding the size of a type* in bytes.
+
+            Console.WriteLine($"Bool: {sizeof(bool)}");
+            Console.WriteLine($"Byte: {sizeof(byte)}");
+            Console.WriteLine($"Signed Byte: {sizeof(sbyte)}");
+            Console.WriteLine($"Char: {sizeof(char)}");
+            Console.WriteLine($"Short: {sizeof(short)}");
+            Console.WriteLine($"Unsigned Short: {sizeof(ushort)}");
+            Console.WriteLine($"Int: {sizeof(int)}");
+            Console.WriteLine($"Unsigned Int: {sizeof(uint)}");
+            Console.WriteLine($"Float: {sizeof(float)}");
+            Console.WriteLine($"Long: {sizeof(long)}");
+            Console.WriteLine($"Unsigned Long: {sizeof(ulong)}");
+            Console.WriteLine($"Double: {sizeof(double)}");
+            Console.WriteLine($"Decimal: {sizeof(decimal)}");
+        }
+    }
+}
